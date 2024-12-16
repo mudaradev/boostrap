a1d8dccd1f85b1e24d591adf115f3464ae116f32
md <mudaradevindi@gmail.com>
2024-12-17 02:25:17 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript for "Next" button to show the sign language text
+document.getElementById("next-btn").addEventListener("click", function() {
+    // Show the sign language text
+    document.getElementById("sign-language-text").style.display = "block";
+});
